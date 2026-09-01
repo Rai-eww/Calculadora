b8b74d96e4088226f87e4bf2c21c4006865d1533
Rai <43397919+Rai-eww@users.noreply.github.com>
2026-09-01 04:07:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ def main ():
 def calcular(operacao):
     try:
         resultado = eval(operacao) 
-        #Eval pega uma String e tentar calcular em formato de expressão.
         print("Resultado: ", resultado)
 
     except ZeroDivisionError: 
-        #Se retornar o erro de "Não é possivel dividir por 0" retornaremos esse print.
         print("Não é possivel dividir por 0.")
     except: 
-        #Caso aconteça outro erro, retornaremos esse print.
         print("Calculo não reconhecido.")
 main()
 
